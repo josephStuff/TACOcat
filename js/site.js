9d6561ef9aea3c4c88839915ad8383adacaf28ee
josephStuff <93104277+josephStuff@users.noreply.github.com>
2022-01-19 23:33:18 -0500

    first save.
    needs a return value.  not sure where

--- js/site.js
+++ js/site.js
@@ -1,14 +1,72 @@
 
 
 
-// display message to the user.
+// ----------PULL USER INPUT----------
 
-function getMessage() {
-    let userMessage = document.getElementById("message").value;
-    Swal.fire(
-        'Button clicked!',
-        userMessage,
-        "success"
-    );
-    console.log(userMessage);
+function getResults() {
+    document.getElementById("alert").classList.add("invisible");    
+    let userString = document.getElementById("theInput").value;
+    let isPalindrome = checkForPalindrome(userString);
+    displayData(isPalindrome);
+    
 }
+
+// -------------------THIS GENERATES DATA -------------
+
+// function isPalindrome (userString) {
+//   let revString = "";
+
+//   for (let i = userString.length - 1; i >= 0; i--) {
+//     revString += userString[i];
+//   }
+
+//   return revString;
+// }
+
+function checkForPalindrome(userString) {
+  userString = userString.toLowerCase();
+
+  let regex = /[^a-z0-9]/gi;
+  userString = userString.replace(regex, "");
+
+  let revString = "";
+  for (let index = userString.length - 1; index >= 0; index--) {
+    revString += userString[index];
+
+  }
+
+  if (revString !== userString) {
+    return false;
+  } else {
+    return true;
+  }
+
+
+}
+
+// ------------------------ DISPLAY THE REVERSED STRING TO PAGE --------
+
+function displayData(isPalindrome) {
+
+        // document.getElementById("alert").classList.remove("alert-success")
+        // document.getElementById("alert").classList.remove("alert-danger");
+
+    if (isPalindrome == true) {
+
+        document.getElementById("msg").innerHTML = `Your string reversed is a Palindrome!`;
+        document.getElementById("alertHeader").innerHTML = "Well Done!" ;
+        document.getElementById("msg").innerHTML = `Your phrase is a Palendrome!` ;
+    }
+    else {
+
+        document.getElementById("msg").innerHTML = `Your string reversed is NOT  a Palindrome!`;
+        document.getElementById("alertHeader").innerHTML = "OH NO!";
+        document.getElementById("msg").innerHTML =  `Your string is NOT a Palindrome` ;
+    }
+
+    
+    //   document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //   document.getElementById("alert").classList.remove("d-none");
+}
+
